59938ef55597b03610d17f6f9310f888960707d3
Niklas Hambüchen <mail@nh2.me>
2025-04-05 01:39:14 +0200

    hetzner-dedicated-wipe-and-install-nixos: Fix permitRootLogin deprecation.
    Deprecated in (at least) NixOS 24.11, maybe earlier.

--- hosters/hetzner-dedicated/hetzner-dedicated-wipe-and-install-nixos.sh
+++ hosters/hetzner-dedicated/hetzner-dedicated-wipe-and-install-nixos.sh
@@ -273,7 +273,9 @@ cat > /mnt/etc/nixos/configuration.nix <<EOF
 
   # Initial empty root password for easy login:
   users.users.root.initialHashedPassword = "";
-  services.openssh.permitRootLogin = "prohibit-password";
+  services.openssh.settings = {
+    PermitRootLogin = "prohibit-password";
+  };
 
   users.users.root.openssh.authorizedKeys.keys = [
     # FIXME Replace this by your SSH pubkey!
