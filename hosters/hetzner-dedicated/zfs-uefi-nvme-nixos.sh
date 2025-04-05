29c6d78f6727b8ead947cf32967dd6984b930694
Niklas Hambüchen <mail@nh2.me>
2025-04-05 02:27:21 +0200

    zfs-uefi-nvme-nixos: Fix incorrect netmasks.
    Like similar previous commit for
    `hetzner-dedicated-wipe-and-install-nixos.sh`.

--- hosters/hetzner-dedicated/zfs-uefi-nvme-nixos.sh
+++ hosters/hetzner-dedicated/zfs-uefi-nvme-nixos.sh
@@ -363,7 +363,12 @@ cat > /mnt/etc/nixos/configuration.nix <<EOF
   networking.interfaces."$NIXOS_INTERFACE".ipv4.addresses = [
     {
       address = "$IP_V4";
-      prefixLength = 24;
+      # Hetzner requires /32, see:
+      #     https://docs.hetzner.com/robot/dedicated-server/network/net-config-debian-ubuntu/#ipv4.
+      # NixOS automatically sets up a route to the gateway
+      # (but only because we set "networking.defaultGateway.interface" below), see
+      #     https://github.com/NixOS/nixops/pull/1032#issuecomment-2763497444
+      prefixLength = 32;
     }
   ];
   networking.interfaces."$NIXOS_INTERFACE".ipv6.addresses = [
@@ -372,7 +377,11 @@ cat > /mnt/etc/nixos/configuration.nix <<EOF
       prefixLength = 64;
     }
   ];
-  networking.defaultGateway = "$DEFAULT_GATEWAY";
+  networking.defaultGateway = {
+    address = "$DEFAULT_GATEWAY";
+    # Interface must be given for Hetzner networking to work, see comment above.
+    interface = "$NIXOS_INTERFACE";
+  };
   networking.defaultGateway6 = { address = "fe80::1"; interface = "$NIXOS_INTERFACE"; };
   networking.nameservers = [
     # cloudflare
